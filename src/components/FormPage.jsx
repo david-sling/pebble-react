095a8b8e6043278625a7a92800de2e91032f3b50
unknown <sddavid.johan@gmail.com>
2021-03-06 17:42:19 +0530

    Added APK download link

--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -31,6 +31,7 @@ export default function FormPage(props) {
                 <div className="type">
                     <img src={Type} alt="pebble-type"/>
                 </div>
+                <a href="/pebble-v1-0-1.apk"><button>Download APK for android</button></a>
             </div>
             <div className="right">
                 <div className="form">
